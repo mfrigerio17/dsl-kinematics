e57bbec94cf3aa16bba25de568f2d435e4407ba4
Marco Frigerio <marco.frigerio@iit.it>
2012-02-07 17:34:01 +0100

    New class to wrap the information associated with a Jacobian matrix

---
+++ ver2/src/iit.dsl/generator/Jacobian.java
@@ -0,0 +1,72 @@
+package iit.dsl.generator;
+
+import iit.dsl.kinDsl.AbstractLink;
+import iit.dsl.kinDsl.Joint;
+import iit.dsl.kinDsl.RefFrame;
+import iit.dsl.kinDsl.Robot;
+
+import java.util.List;
+
+public class Jacobian {
+    private final Robot robot;
+    private final RefFrame baseFrame;
+    private final RefFrame movingFrame;
+    private final AbstractLink baseLink;
+    private final AbstractLink movingLink;
+    private final List<AbstractLink> linksChain;
+    private final List<Joint> jointsChain;
+    private final int cols;
+
+    private final Common common;
+
+    public Jacobian(final Robot robot, final iit.dsl.transspecs.transSpecs.FramePair jspec) {
+        this.common = new Common();
+        this.robot = robot;
+        this.baseFrame   = common.getFrameByName(robot, jspec.getBase().getName());
+        this.movingFrame = common.getFrameByName(robot, jspec.getTarget().getName());
+        this.baseLink = common.getContainingLink(robot, baseFrame);
+        this.movingLink = common.getContainingLink(robot, movingFrame);
+        this.linksChain = common.buildChain(baseLink, movingLink);
+        this.jointsChain = common.getChainJoints(linksChain);
+        this.cols = jointsChain.size();
+    }
+
+    public Robot getRobot() {
+        return robot;
+    }
+
+    public RefFrame getBaseFrame() {
+        return baseFrame;
+    }
+
+    public RefFrame getMovingFrame() {
+        return movingFrame;
+    }
+
+    public AbstractLink getBaseLink() {
+        return baseLink;
+    }
+
+    public AbstractLink getMovingLink() {
+        return movingLink;
+    }
+
+    public List<AbstractLink> getLinksChain() {
+        return linksChain;
+    }
+
+    public int getCols() {
+        return cols;
+    }
+
+    public List<Joint> getJointsChain() {
+        return jointsChain;
+    }
+
+    /**
+     * Creates a default name for this Jacobian */
+    public String getName() {
+        return baseFrame.getName() + "_J_" + movingFrame.getName();
+    }
+
+}
