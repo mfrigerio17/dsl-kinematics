76a0b0919d80735811475ba44489b760749b4b7c
Marco Frigerio <marco.frigerio@iit.it>
2012-02-16 17:07:11 +0100

    New implementation of the Maxima-conversion-specification, which implements the new interface in 'iit.dsl.maxdsl.generator.cpp' instead of the old one in 'iit.dsl.coord.generator.cpp'.

---
+++ ver2/src/iit.dsl/generator/cpp/MaximaReplacementSpec.java
@@ -0,0 +1,36 @@
+package iit.dsl.generator.cpp;
+
+import iit.dsl.kinDsl.Joint;
+import iit.dsl.kinDsl.Robot;
+import iit.dsl.maxdsl.maximaDsl.VarLiteral;
+
+public class MaximaReplacementSpec implements iit.dsl.maxdsl.generator.IIdentifiersReplacement
+{
+    private final iit.dsl.generator.Common utils;
+    private final Robot robot;
+
+    MaximaReplacementSpec(Robot rob) {
+        this.robot = rob;
+        this.utils = new iit.dsl.generator.Common();
+    }
+    @Override
+    public String variableStr(VarLiteral argument) {
+        Joint j = utils.getJointFromVariableName(robot, argument.getValue().getName());
+        if(j == null) {
+            throw new RuntimeException("Did not find any joint corresponding to variable " +
+                    argument.getValue().getName() + " in robot " + robot.getName());
+        }
+        return Common.valueAccessorOf(j).toString();
+    }
+
+    @Override
+    public String cosineStr() {
+        return "std::cos";
+    }
+
+    @Override
+    public String sineStr() {
+        return "std::sin";
+    }
+
+}
