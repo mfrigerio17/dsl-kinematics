e0d4d393801a05b42f2df3e374ccbe9886812593
Marco Frigerio <marco.frigerio@iit.it>
2012-03-25 19:33:48 +0200

    Some cleaning

--- ver2/src/iit.dsl/TransformsAccessor.java
+++ ver2/src/iit.dsl/TransformsAccessor.java
@@ -3,33 +3,32 @@ package iit.dsl;
 import java.io.File;
 
 import iit.dsl.coord.coordTransDsl.Model;
+import iit.dsl.generator.FramesTransforms;
 import iit.dsl.kinDsl.Robot;
 
-import com.google.inject.Inject;
 import com.google.inject.Injector;
-import com.google.inject.Provider;
 
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.xtext.resource.XtextResourceSet;
 
 public class TransformsAccessor {
-    @Inject
-    Provider<ResourceSet> resourceSetProvider;
-
     private Resource resource;
-    private ResourceSet set;
+    private final ResourceSet set;
 
     private static Injector injector = new iit.dsl.coord.CoordTransDslStandaloneSetupGenerated().createInjectorAndDoEMFRegistration();
 
+    public TransformsAccessor() {
+        set = injector.getInstance(XtextResourceSet.class);
+    }
+
     public iit.dsl.coord.coordTransDsl.Model getTransformsModel(Robot robot) {
-        set = resourceSetProvider.get();
-        resource = set.getResource(URI.createURI("generated_code/misc/"+robot.getName() + ".ctdsl"), true);
+        resource = set.getResource(URI.createURI("generated_code/misc/"+ FramesTransforms.fileName(robot)), true);
         return (Model)resource.getContents().get(0);
     }
 
     public iit.dsl.coord.coordTransDsl.Model getTransformsModel(Robot robot, File modelFile) {
-        set = resourceSetProvider.get();
         if(modelFile.isFile()) {
             resource = set.getResource(URI.createURI(modelFile.getAbsolutePath()), true);
             return (Model)resource.getContents().get(0);
