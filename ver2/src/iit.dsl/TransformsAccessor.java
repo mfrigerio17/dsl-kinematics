0182ad56d6d0a48295b4268c105921117e0ad3f4
Marco Frigerio <marco.frigerio@iit.it>
2012-04-19 23:27:52 +0200

    Cosmetic changes

--- ver2/src/iit.dsl/TransformsAccessor.java
+++ ver2/src/iit.dsl/TransformsAccessor.java
@@ -10,21 +10,25 @@ import com.google.inject.Injector;
 
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.xtext.resource.XtextResource;
 import org.eclipse.xtext.resource.XtextResourceSet;
 
 public class TransformsAccessor {
-    private Resource resource;
-    private final ResourceSet set;
-
-    private static Injector injector = new iit.dsl.coord.CoordTransDslStandaloneSetupGenerated().createInjectorAndDoEMFRegistration();
+    private static Injector injector =
+            new iit.dsl.coord.CoordTransDslStandaloneSetupGenerated()
+                .createInjectorAndDoEMFRegistration();
+    private Resource resource = null;
+    private XtextResourceSet set = null;
 
     public TransformsAccessor() {
         set = injector.getInstance(XtextResourceSet.class);
+        set.addLoadOption(XtextResource.OPTION_RESOLVE_ALL, Boolean.TRUE);
     }
 
     public iit.dsl.coord.coordTransDsl.Model getTransformsModel(Robot robot) {
-        resource = set.getResource(URI.createURI("generated_code/misc/"+ FramesTransforms.fileName(robot)), true);
+        resource = set.getResource(
+                URI.createURI(
+                        "generated_code/misc/"+ FramesTransforms.fileName(robot)), true);
         return (Model)resource.getContents().get(0);
     }
 
