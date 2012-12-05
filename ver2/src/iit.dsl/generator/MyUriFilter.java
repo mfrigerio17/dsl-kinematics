5303c952f54e6c457051b76424275f31b20f8392
Marco Frigerio <marco.frigerio@iit.it>
2012-12-05 21:23:27 +0100

    possibly matches two source files, instead of one

--- ver2/src/iit.dsl/generator/MyUriFilter.java
+++ ver2/src/iit.dsl/generator/MyUriFilter.java
@@ -5,10 +5,12 @@ import org.eclipse.xtext.mwe.UriFilter;
 
 public class MyUriFilter implements UriFilter {
 
-    private final String match = "/home/phd/work/eclipse_workspace/kindsl/models/hyq_fixed.kindsl";
+    private final String match1 = "/home/phd/work/eclipse_workspace/kindsl/models/hyq_fixed.kindsl";
+    private final String match2 = "/home/phd/work/eclipse_workspace/kindsl/models/hyq.kindsl";
     @Override
     public boolean matches(URI uri) {
-        if(match.equals(uri.toFileString())) {
+        String file = uri.toFileString();
+        if(match1.equals(file) || match2.equals(file)) {
             return true;
         }
         return false;
