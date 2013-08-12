c4b54720f06980564ad41a2152edc8ad714519d4
Marco Frigerio <marco.frigerio@iit.it>
2013-08-12 19:52:47 +0200

    Misc changes in the Names class:
    - specific subclass for type-names in the iit-rbd library
    
    - added name for the header file with stuff related to parameters
    
    - added methods to return the name of the container classes for transforms
    (cleaned old stuff related to the namespaces for transforms); they
    use the new configurator interface to get the values
    
    - the configurators are set with default objects, so the checks for
    null pointers and the hardcoded default values of the various
    names have been removed

--- src/iit.dsl/generator/cpp/Names.java
+++ src/iit.dsl/generator/cpp/Names.java
@@ -1,8 +1,8 @@
 package iit.dsl.generator.cpp;
 
-import java.util.Arrays;
 import java.util.List;
 
+import iit.dsl.generator.cpp.config.DefaultConfigurator;
 import iit.dsl.generator.cpp.config.IConfigurator;
 
 import iit.dsl.kinDsl.Robot;
@@ -10,61 +10,92 @@ import iit.dsl.kinDsl.Robot;
 public class Names {
     private static IConfigurator.Names.Files files = null;
     private static IConfigurator.Names.Namespaces nspaces = null;
+    private static IConfigurator.Names.ClassesAndTypes types = null;
 
-    public static void setConfigurators(IConfigurator.Names.Files filesCfg) {
+    static {
+        DefaultConfigurator config = new DefaultConfigurator();
+        setConfigurators(config, config, config);
+    }
+
+    public static void setConfigurators(IConfigurator.Names.Files filesCfg)
+    {
+        if(filesCfg == null) {
+            //TODO log warning, refusing to set null configurator
+            return;
+        }
         files = filesCfg;
     }
-    public static void setConfigurators(IConfigurator.Names.Namespaces nspacesCfg) {
+    public static void setConfigurators(IConfigurator.Names.Namespaces nspacesCfg)
+    {
+        if(nspacesCfg == null) {
+            //TODO log warning, refusing to set null configurator
+            return;
+        }
         nspaces = nspacesCfg;
     }
+    public static void setConfigurators(IConfigurator.Names.ClassesAndTypes typesCfg)
+    {
+        if(typesCfg == null) {
+            //TODO log warning, refusing to set null configurator
+            return;
+        }
+        types = typesCfg;
+    }
+
 
     public static void setConfigurators(
             IConfigurator.Names.Files filesCfg,
-            IConfigurator.Names.Namespaces nspacesCfg)
+            IConfigurator.Names.Namespaces nspacesCfg,
+            IConfigurator.Names.ClassesAndTypes typesCfg)
     {
         files = filesCfg;
         nspaces = nspacesCfg;
+        types = typesCfg;
     }
 
     static public class Files {
         static public String folder(Robot robot) {
-            return (files == null? robot.getName().toLowerCase() : files.folder(robot));
+            return files.folder(robot);
         }
         static public String mainHeader(Robot robot) {
-            return (files == null? "declarations" : files.h_declarations(robot));
+            return files.h_declarations(robot);
         }
         static public String linkDataMapHeader(Robot robot) {
-            return (files == null? "link_data_map" : files.h_linkDataMap(robot));
+            return files.h_linkDataMap(robot);
         }
         static public String jointDataMapHeader(Robot robot) {
-            return (files == null? "joint_data_map" : files.h_jointDataMap(robot));
+            return files.h_jointDataMap(robot);
         }
         static public String transformsHeader(Robot robot) {
-            return (files == null? "transforms" : files.h_transforms(robot));
+            return files.h_transforms(robot);
         }
+        static public String parametersHeader(Robot robot) {
+            return files.h_parameters(robot);
+        }
+
         static public String jacobiansHeader(Robot robot) {
-            return (files == null? "jacobians" : files.h_jacobians(robot));
+            return files.h_jacobians(robot);
         }
         static public String transformsSource(Robot robot) {
-            return (files == null? "transforms" : files.src_transforms(robot));
+            return files.src_transforms(robot);
         }
 
 
         static public class RBD {
             static public String header(Robot r) {
-                return (files == null? "inverse_dynamics" : files.h_invdyn(r));
+                return files.h_invdyn(r);
             }
             static public String source(Robot r) {
-                return (files == null? "inverse_dynamics" : files.src_invdyn(r));
+                return files.src_invdyn(r);
             }
             static public String jsimHeader(Robot r) {
-                return (files == null? "jsim" : files.h_jsim(r));
+                return files.h_jsim(r);
             }
             static public String inertiaHeader(Robot r) {
-                return (files == null? "inertia_params" : files.h_inertias(r));
+                return files.h_inertias(r);
             }
             static public String inertiaSource(Robot r) {
-                return (files == null? "inertia_params" : files.src_inertias(r));
+                return files.src_inertias(r);
             }
 
 
@@ -91,26 +122,16 @@ public class Names {
     }
 
     static public class Namespaces {
-        static public String transforms6D =
-                nspaces==null? "motion_transf" : nspaces.T6D_motion();
-        static public String T6D_force =
-                nspaces==null? "force_transf" :  nspaces.T6D_force();
-        static public String THomogeneous =
-                nspaces==null? "homogeneous_transf" : nspaces.THomogeneous();
-
         static public List<String> enclosing() {
-            return
-            nspaces==null?
-                    Arrays.asList(new String[]{"iit"}) :
-                        nspaces.enclosing();
+            return nspaces.enclosing();
         }
 
         static public String rob(Robot r) {
-            return nspaces==null? r.getName() : nspaces.robot(r);
+            return nspaces.robot(r);
         }
 
-        static public String dynamics  = nspaces==null? "dyn"  : nspaces.dynamics();
-        static public String jacobians = nspaces==null? "jacs" : nspaces.jacobians();
+        static public String dynamics  = nspaces.dynamics();
+        static public String jacobians = nspaces.jacobians();
         static public String internal = "internal";
 
         static public class Qualifiers {
@@ -121,7 +142,6 @@ public class Names {
                 return robot(rob) + "::" + jacobians;
             }
             static public String iit_rbd() {
-                if(nspaces==null) return "iit::rbd";
                 String ret = "";
                 List<String> names = nspaces.iit_rbd();
                 int len = names.size();
@@ -137,16 +157,39 @@ public class Names {
 
     static public class Types {
         static public String jointState = "JointState";
-        public static String jstateDependentMatrix(Robot model, int matrixSize) {
-            return "iit::rbd::JStateDependentMatrix<" + jointState + ", " + matrixSize + ", " + matrixSize + ">";
-        }
-        public static String jstateDependentMatrix() {
-            return "iit::rbd::JStateDependentMatrix";
-        }
+
         public static String jacobianLocal = "JacobianT";
-        public static String jspaceMLocal = "JSpaceInertiaMatrix";
+        public static String jspaceMLocal = "JSIM";
         public static String vector3d = Namespaces.Qualifiers.iit_rbd() + "::Vector3d";
         public static String extForces = "ExtForces";
+
+        /**
+         * The names of the classes that contain the various coordinate transforms.
+         * The code of such classes is generated by generators in the Transforms-DSL
+         * package
+         * @author Marco Frigerio
+         */
+        static public class Transforms {
+            static public String homogeneous() {
+                return types.transforms_homogeneous();
+            }
+            static public String spatial_motion() {
+                return types.transforms_spatial_motion();
+            }
+            static public String spatial_force() {
+                return types.transforms_spatial_force();
+            }
+        }
+
+        static public class IIT_RBD {
+            static public String stateDependentMatrix(
+                    String state, String rows, String cols, String matrix) {
+                return "StateDependentMatrix<" +state+ ", " +rows+ ", " +cols+ ", " +matrix+ ">";
+            }
+            static public String jacobianBase(String state, String cols, String matrix) {
+                return "JacobianBase<" + state + ", " + cols + ", " + matrix + ">";
+            }
+        }
     }
 
     static public class GlobalVars {
